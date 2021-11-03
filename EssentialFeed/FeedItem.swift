1be7c33fbb1e4000f62586bc916a4f8deccfd099
Rishav Gupta <gupta.rishav2608@gmail.com>
2021-11-03 06:01:23 +0530

    created feed Item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Rishav Gupta on 03/11/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
