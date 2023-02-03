84cb44020a30f35e33305d88f13dcbd46ec000ad
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-02-03 06:05:12 +0530

    creates URLSessionDataTask with url on URLSessionHTTPClient.get(from: URL)

---
+++ EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,51 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Rishav Gupta on 03/02/23.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        // as URLSession does not have receivedURLs, as it is a test detail. We need to spy it to create a URLSessionSpy which will be a subclass of URLSession
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK :- Helpers
+
+    // when  we are subclassing URLSession and URLSessionDataTask, it is often dangerous as
+    //  we do not own those classes, we do not have access to their implementations
+    // if we start mocking classes we do not own we can start creating assumptions in our mocked behaviour that could be wrong.
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            // now this method returns a URLSessionDataTask but we never want to ever execute a network request during a test. We need some sort of mock implementation of that DataTask, like a fake URLSessionDataTask
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask { }
+}
