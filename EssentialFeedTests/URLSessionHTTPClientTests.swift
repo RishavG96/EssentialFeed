b8d2cb9dd9cd7bd929084b5a75d4f00107e4aec9
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-02-04 11:30:43 +0530

    delivers failure with error on request error

--- EssentialFeedTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class URLSessionHTTPClient {
     private let session: URLSession
@@ -14,22 +15,26 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }.resume()
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> ()) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
 
     func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        // as URLSession does not have receivedURLs, as it is a test detail. We need to spy it to create a URLSessionSpy which will be a subclass of URLSession
-        
-        let sut = URLSessionHTTPClient(session: session)
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
+//        let url = URL(string: "http://any-url.com")!
+//        let session = URLSessionSpy()
+//        // as URLSession does not have receivedURLs, as it is a test detail. We need to spy it to create a URLSessionSpy which will be a subclass of URLSession
+//
+//        let sut = URLSessionHTTPClient(session: session)
+//        sut.get(from: url) { _ in }
+//
+//        XCTAssertEqual(session.receivedURLs, [url])
     } // we checked here data task is getting created with the right url, next thing we need to check for the dataTask to start is to call resume()
     
     func test_getFromURL_resumesDataTaskWithURL() {
@@ -39,9 +44,31 @@ class URLSessionHTTPClientTests: XCTestCase {
         session.stub(url: url, task: task)
         
         let sut = URLSessionHTTPClient(session: session)
-        sut.get(from: url)
+        sut.get(from: url) { _ in }
         
         XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
+    func test_getFromURL_failsOnRequestError() {
+        let url = URL(string: "http://any-url.com")!
+        let error = NSError(domain: "any error", code: 1)
+        let session = URLSessionSpy()
+        session.stub(url: url, error: error)
+        
+        let exp = expectation(description: "wait for completion")
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(receivedError as NSError):
+                XCTAssertEqual(receivedError, error)
+            default:
+                XCTFail("Expected failure with error got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK :- Helpers
@@ -51,16 +78,25 @@ class URLSessionHTTPClientTests: XCTestCase {
     // if we start mocking classes we do not own we can start creating assumptions in our mocked behaviour that could be wrong.
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
-        private var stubs = [URL: URLSessionDataTask]()
+        private var stubs = [URL: Stub]()
         
-        func stub(url: URL, task: URLSessionDataTask) {
-            stubs[url] = task
+        private struct Stub {
+            let task: URLSessionDataTask
+            let error: Error?
+        }
+        
+        func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+            stubs[url] = Stub(task: task, error: error)
         }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
             // now this method returns a URLSessionDataTask but we never want to ever execute a network request during a test. We need some sort of mock implementation of that DataTask, like a fake URLSessionDataTask
-            return stubs[url] ?? FakeURLSessionDataTask()
+            guard let stub = stubs[url] else {
+                fatalError("couldn't find stub for the given url")
+            }
+            completionHandler(nil, nil, stub.error)
+            return stub.task
         }
     }
     
